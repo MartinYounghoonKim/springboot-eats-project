9456ce42924c5f803436372e25e17d196d9392ce
martinyounghoonkim <hoons0131@gmail.com>
2020-08-27 12:54:59 +0900

    aa

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -1,9 +1,10 @@
 package kr.co.fastcampus.eatgo.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
 	List<MenuItem> findAllByRestaurantId(Long id);
 }
