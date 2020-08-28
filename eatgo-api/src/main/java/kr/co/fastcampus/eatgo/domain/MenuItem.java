45e5beab37b33b973c98039b8c0a208050ecdf93
martinyounghoonkim <hoons0131@gmail.com>
2020-08-28 12:49:59 +0900

    add

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,12 +1,16 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class MenuItem {
 
 	@Id
+	@GeneratedValue
 	private Long id;
 	private Long restaurantId;
 	private final String name;
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -38,7 +39,7 @@ class RestaurantServiceTest {
 		Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
 		restaurants.add(restaurant);
 		given(restaurantRepository.findAll()).willReturn(restaurants);
-		given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+		given(restaurantRepository.findById(1004L)).willReturn(Optional.of(restaurant));
 	}
 
 	private void mockMenuItemRepository () {
